7c7b0e22555bc52ee8d22b4a69a4e0bca2a88e42
Carlos Tojal <carlos.tojal@hotmail.com>
2020-05-30 18:43:27 +0100

    utility functions

--- helpers.h
+++ helpers.h
@@ -1,4 +1,8 @@
 
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
 //
 // Carlos Tojal, 2020
 // Hangman
@@ -21,4 +25,33 @@ void to_uppercase(char* s) {
 		if(s[i] >= 97)
 			s[i] -= 32;
 	}
+}
+
+char get_random_char() {
+	char c;
+	time_t t;
+	srand((unsigned) time(&t));
+	do {
+		c = rand() % 91; // get random character from ASCII code 0 to 90
+	} while(c < 65); // repeat while the character is not in A-Z
+}
+
+void draw_game(char* word, int word_size) {
+	int i;
+	for(i = 0; i < word_size; i++) {
+		if(word[i] == (int) ' ')
+			printf("_ ");
+		else
+			printf("%c ", word[i]);
+	}
+	printf("\n");
+}
+
+int guess_word_len(char* word, int word_size) {
+	int len = 0;
+	for(int i = 0; i < word_size; i++) {
+		if(word[i] != ' ')
+			len++;
+	}
+	return len;
 }
--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 int main() {
 
 	char word[20];
+	char guess_word[20];
 	printf("** Hangman **\n\n");
 	
 	// get the word
@@ -17,8 +18,16 @@ int main() {
 		printf("Enter your word: ");
 		scanf("%s", word);
 		to_uppercase(word);
-		printf("%s\n", word);
 	} while(!check_word(word));
+
+	// initialize empty guess word
+	for(int i = 0; i < strlen(word); i++)
+		guess_word[i] = ' ';
+	
+	do {
+		// draw the game
+		draw_game(guess_word, strlen(word));
+	} while(guess_word_len(guess_word, strlen(word)) != strlen(word));
 
 	return 0;
 }
